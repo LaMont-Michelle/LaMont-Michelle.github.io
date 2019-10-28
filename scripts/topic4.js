ebc17104bb4ec840367511a14bc7908705f264ca
shellelamont <lam16017@byui.edu>
2019-10-28 14:28:17 -0600

    topic4 update

--- scripts/topic4.js
+++ scripts/topic4.js
@@ -2,18 +2,18 @@
 // AJAX FOR TOPIC 4
 //  using the XMLHttpRequest() function 
 function requestDisplay() {
-    var xhttp = new XMLHttpRequest();       // commonly used variable 
-    var results = document.getElementById('displayParsed');
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            var data = JSON.parse(xhttp.responseText);
-            for (i = 0; i < 25; i++) {
-                results.innerHTML += '<li>' + "font-family: " + data.items[i].family + ", " +
-                    "category: " + data.items[i].category + ", " + "version: " + data.items[i].version + ", " + "last modified: " + data.items[i].lastModified + '</li>';
-                console.log(data);
-            }
+var xhttp = new XMLHttpRequest();       // xhttp is a commonly used variable 
+var results = document.getElementById('displayParsed');
+xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        var data = JSON.parse(xhttp.responseText);
+        for (i = 0; i < 25; i++) {
+            results.innerHTML += '<li>' + "font-family: " + data.items[i].family + ", " +
+                "category: " + data.items[i].category + ", " + "version: " + data.items[i].version + ", " + "last modified: " + data.items[i].lastModified + '</li>';
+            console.log(data);
         }
     }
+}
     xhttp.open("GET", "https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyDGCSG48HaUElIeULsDvk5XyHfSh7gNGNM", true);
     xhttp.send();
 };
