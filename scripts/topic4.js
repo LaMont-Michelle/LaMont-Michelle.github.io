5643eb180b36810a8869c78ff0a1f1ad792063b7
shellelamont <lam16017@byui.edu>
2019-10-30 00:49:59 -0600

    topic4 update

--- scripts/topic4.js
+++ scripts/topic4.js
@@ -17,3 +17,26 @@ xhttp.onreadystatechange = function () {
     xhttp.open("GET", "https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyDGCSG48HaUElIeULsDvk5XyHfSh7gNGNM", true);
     xhttp.send();
 };
+
+function requestDisplaySW() {
+var xhttp = new XMLHttpRequest();       // xhttp is a commonly used variable 
+var results = document.getElementById('displayParsedSW');
+xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        var dataSW = JSON.parse(xhttp.responseText);
+        console.log(dataSW);
+       results.innerHTML += 
+       "Name: " + dataSW.name + 
+       '<br>' + "Height: " + dataSW.height +
+       '<br>' + "Mass: " + dataSW.mass + 
+       '<br>' + "Hair color: " + dataSW.hair_color +
+       '<br>' + "Skin color: " + dataSW.skin_color +
+       '<br>' + "Eye color: " + dataSW.eye_color +
+       '<br>' + "Birth year: " + dataSW.birth_year +
+       '<br>' + "Gender: " + dataSW.gender ;
+       
+        }
+    }
+    xhttp.open("GET", "https://swapi.co/api/people/1/", true);
+    xhttp.send();
+};
